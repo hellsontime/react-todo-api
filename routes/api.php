ecaf7b92fb6b60e4afc341e60bdf41508af9922f
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-30 15:46:11 +0300

    Api routes refactor

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v1"], function () {
+
+    // Public routes
+//    Route::post('register/', [AuthController::class, 'register'])->name('auth.register');
+//    Route::post('login/', [AuthController::class, 'login'])->name('auth.login');
+
+    // Protected routes
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        //
+
+    });
 });
