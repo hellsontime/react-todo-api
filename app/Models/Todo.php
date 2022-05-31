34bc1a2ae3413c76a600daa3be88d5cb74fec856
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-05-31 12:02:19 +0300

    Todo model + factory implementation

---
+++ app/Models/Todo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends BaseModel
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "id");
+    }
+}
